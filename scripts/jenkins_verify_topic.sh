8180d0e98af39163a662e577359ad46b6f74d07e
chasinglulu <wangkartx@gmail.com>
2024-05-30 15:31:17 +0800

    add scripts/jenkins_verify_topic.sh

--- scripts/jenkins_verify_topic.sh
+++ scripts/jenkins_verify_topic.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
-set +x
+# 参数格式化
+TOPIC_NAME=$(echo $TOPIC_NAME | sed 's/，/,/g' | sed 's/,//')
 
-# main start
-export GERRIT_ID=$(echo ${GERRIT_ID} | sed 's/ //g' | sed 's/，/,/g')
-echo ${GERRIT_ID}
+ASSIGN_TOPIC=$(echo $ASSIGN_TOPIC | sed 's/，/,/g' | sed 's/,//')
+if [ -n "$ASSIGN_TOPIC" ]
+then
+	TIMESTAMPS=$(date '+%M%S')
+	NEW_TOPIC=${ASSIGN_TOPIC}_${TIMESTAMPS}
+fi
+
+if [ "$PATCH_MODE" = "Topic" ]
+then
+	# Get all the gerrit IDs from the same topic
+	GERRIT_ID=$(ssh -p 29418 gerrit.aixin-chip.com gerrit query --patch-sets topic:${TOPIC_NAME} < /dev/null | grep '^  number:' | awk '{print $2}' | sort | xargs )
+	echo "GERRIT_ID: $GERRIT_ID"
+elif [ "$PATCH_MODE" = "GerritID" ]
+then
+	GERRIT_ID=$(echo ${GERRIT_ID} | sed 's/ //g' | sed 's/，/,/g' | sed 's/,/ /g' )
+	echo "GERRIT_ID: $GERRIT_ID"
+else
+	echo "没有合适的patch格式"
+fi
 
 # add patch set ids:
-for id in $(echo ${GERRIT_ID} | sed 's/,/ /g')
+for id in ${GERRIT_ID}
 do
-    PATCH_ID_HTML="${PATCH_ID_HTML} <a href='https://gerrit.aixin-chip.com/${id}'>${id}</a>"
+	PATCH_ID_HTML="${PATCH_ID_HTML} <a href='https://gerrit.aixin-chip.com/${id}'>${id}</a>"
 done
 
 # set job start by who
@@ -17,38 +34,46 @@ BUILD_CAUSE_JSON=$(curl -u jenkins:G}4J6F{ObP --silent ${BUILD_URL}/api/json | t
 BUILD_USER_ID=$(echo $BUILD_CAUSE_JSON | tr "," "\n" | grep "userId" | awk -F\" '{print $4}')
 BUILD_USER_NAME=$(echo $BUILD_CAUSE_JSON | tr "," "\n" | grep "userName" | awk -F\" '{print $4}')
 curl -s -u "${SW_JENKINS_USER}":"${SW_JENKINS_TOKEN}" --data-urlencode "description=Gerrit Ids: ${PATCH_ID_HTML}<br>Started by: ${BUILD_USER_NAME}" --data-urlencode "Submit=Submit" "${BUILD_URL}/submitDescription"
+cd "${WORKSPACE}"
+manifest_file="${WORKSPACE}"/.repo/manifests/"${MANIFEST_XML}"
+
 repo forall -c git lfs pull
 set +e
+repo forall -c git lfs pull
 repo manifest -r -o manifest.xml
 cp manifest.xml "${WORKSPACE}"/build/manifest.xml
 
 # cherry-pick change ids
-if [ "${GERRIT_ID}x" = "x" ]; then
-    echo "Gerrit Change ID is empty, exit task!"
-    exit 1
+if [ -z "${GERRIT_ID}" ]
+then
+	echo "*** ERROR *** Gerrit change ID is empty, exit task!"
+	exit 1
 fi
 
 function get_change_patchset_revision {
-    change_revision=$(gerrit -i "${1}" -m get_change_patchset_revision)
-    if [ -n "${change_revision}" ]; then
-        echo "${1}":"${change_revision}" >> "${WORKSPACE}"/change_revision
-    fi
+	change_revision=$(gerrit -i "${1}" -m get_change_patchset_revision)
+	if [ -n "${change_revision}" ]
+	then
+		echo "${1}":"${change_revision}" >> "${WORKSPACE}"/change_revision
+	fi
 }
+
 function lfs_change_cherry_pick() {
-    # must git fetch first, otherwise git show "${change_revision}" failed
-    git fetch ssh://jenkins@gerrit.aixin-chip.com:29418/"${gerrit_id_project}" "${gerrit_id_refId}" && git cherry-pick FETCH_HEAD
-    change_revision=$(echo "${change_revision}" | sed 's/\"//g')
-    lfs_exist=$(git show "${change_revision}" | grep "https://git-lfs.github.com")
-    if [ -n "${lfs_exist}" ]; then
-        remote_name=$(git remote -v | awk '{print $1}' | head -n1)
-        git_branch=$(git branch -a | grep "\->" | awk -F "/" '{print $NF}')
-        if [ -n "${remote_name}" ] && [ -n "${git_branch}" ]; then
-            git remote remove "${remote_name}"
-            git remote add "${remote_name}" ssh://jenkins@gerrit.aixin-chip.com:29418/"${gerrit_id_project}"
-            git reset --hard HEAD
-            git lfs pull "${remote_name}"
-        fi
-    fi
+	# must git fetch first, otherwise git show "${change_revision}" failed
+	git fetch ssh://jenkins@gerrit.aixin-chip.com:29418/"${gerrit_id_project}" "${gerrit_id_refId}" && git cherry-pick FETCH_HEAD
+	change_revision=$(echo "${change_revision}" | sed 's/\"//g')
+	lfs_exist=$(git show "${change_revision}" | grep "https://git-lfs.github.com")
+	if [ -n "${lfs_exist}" ]
+	then
+		remote_name=$(git remote -v | awk '{print $1}' | head -n1)
+		git_branch=$(git branch -a | grep "\->" | awk -F "/" '{print $NF}')
+		if [ -n "${remote_name}" ] && [ -n "${git_branch}" ]; then
+			git remote remove "${remote_name}"
+			git remote add "${remote_name}" ssh://jenkins@gerrit.aixin-chip.com:29418/"${gerrit_id_project}"
+			git reset --hard HEAD
+			git lfs pull "${remote_name}"
+		fi
+	fi
 }
 
 for gerrit_id in $(echo ${GERRIT_ID} | sed 's/,/ /g')
@@ -117,8 +142,51 @@ do
             fi
             echo -e "\n\n"
         fi
-    done < "${WORKSPACE}"/manifest.xml
+    done < "${WORKSPACE}"/build/manifest.xml
 done
+
+function check_topic_exist
+{
+	# gerrit id个数小于2个时，直接退出；
+	# 检查所有的gerrit id是否都存在topic，如有一个存在，退出检查；
+	if [ $# -gt 1 ];then
+		for i in $@
+		do
+			# api_get_topic=$(curl -s -X GET https://gerrit.aixin-chip.com/a/changes/$i/topic --user 'jenkins:G}4J6F{ObP')
+			api_get_topic=$(gerrit -m get_topic -i $i)
+			topic_filter=$(echo $api_get_topic | cut -d '"' -f 2)
+			if [ -n "${topic_filter}" ];then
+				# topic已有值，则退出
+				flag=110 && break
+			fi
+		done
+		flag=119
+	else
+		flag=120
+	fi
+}
+
+function set_topic
+{
+	if [ -n "${ASSIGN_TOPIC}" ]
+	then
+		echo "start update topic"
+		check_topic_exist ${GERRIT_ID}
+		if [ $flag -eq 110 ];then
+			echo "gerri_id:$i has topic: $topic_filter, skip push topic"
+		elif [ $flag -eq 120 ];then
+			echo "单个gerrit无需提交topic"
+		elif [ $flag -eq 119 ];then
+			for i in ${GERRIT_ID}
+			do
+				echo $i
+				curl -X PUT -H "Content-Type: application/json" -d "{"topic": "$new_topic"}" https://gerrit.aixin-chip.com/a/changes/$i/topic --user 'jenkins:G}4J6F{ObP'
+			done
+		fi
+	else
+		echo "Don't need to set topic."
+	fi
+}
 
 if [ "${AXERA_FPGA}" = "true" ]
 then
@@ -129,6 +197,8 @@ if [ "${QEMU_VIRT}" = "true" ]
 then
 BUILD_TARGET="${BUILD_TARGET} qemu_virt"
 fi
+
+echo "============================ $BUILD_TARGET ======================="
 
 export BUILD_TARGET
 export WORKSPACE
@@ -142,3 +212,8 @@ fi
 
 echo "======================== Begin verifying ========================"
 sh ${WORKSPACE}/build/scripts/jenkins_verify.sh
+
+if [ "$PATCH_MODE" = "GerritID" ]
+then
+	set_topic
+fi
