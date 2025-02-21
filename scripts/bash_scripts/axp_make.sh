1af1382de13c91ec498f7f8c75213ea62767ac45
chasinglulu <wangkart@aliyun.com>
2025-02-21 11:17:15 +0800

    add file check for axp_make.sh

--- scripts/bash_scripts/axp_make.sh
+++ scripts/bash_scripts/axp_make.sh
@@ -231,13 +231,14 @@ initialize_image_paths() {
     fi
 
     # Check if files exist
-    # paths=(
-    #  "$EIP_PATH" "$FDL1_PATH" "$FDL2_PATH" "$SPL_PATH" "$UBOOT_PATH"
-    #  "$KERNEL_PATH" "$ATF_PATH" "$OPTEE_PATH"
-    # )
-    # for path in "${paths[@]}"; do
-    #  [ -n "$path" ] && [ ! -f "$path" ] && echo "File not found: $path"
-    # done
+    paths=(
+     "$EIP_PATH" "$FDL1_PATH" "$FDL2_PATH" "$SPL_PATH" "$UBOOT_PATH"
+     "$KERNEL_PATH" "$ATF_PATH" "$OPTEE_PATH"
+    )
+    for path in "${paths[@]}"; do
+      [ -n "$path" ] && [ ! -f "$path" ] && echo "File not found: $path"
+    done
+    debug "Image paths initialized."
 }
 
 initialize_project_specific_paths() {
